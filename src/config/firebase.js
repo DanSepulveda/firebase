bebba30b2362a554712a85eff381e6cf82047b15
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2022-09-24 07:18:04 -0300

    lalala

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,10 +3,10 @@ import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: "fb-crud-93570.firebaseapp.com",
-  projectId: "fb-crud-93570",
-  storageBucket: "fb-crud-93570.appspot.com",
-  messagingSenderId: "1004097450473",
+  authDomain: process.env.REACT_APP_AUTHO_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: process.env.REACT_APP_SENDER,
   appId: process.env.REACT_APP_ID
 };
 
