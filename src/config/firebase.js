e4b09d91312d342905b73222c585106a9fa38d2c
Daniel Sepulveda <d.sepulveda.perez@gmail.com>
2022-09-24 07:28:29 -0300

    hola

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,8 +8,14 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE,
   messagingSenderId: process.env.REACT_APP_SENDER,
   appId: process.env.REACT_APP_ID
+  //   apiKey: "AIzaSyD8mk81mE_42F3mHEzfMxmrBTsvzlZPqRM",
+  // authDomain: "fb-crud-93570.firebaseapp.com",
+  // projectId: "fb-crud-93570",
+  // storageBucket: "fb-crud-93570.appspot.com",
+  // messagingSenderId: "1004097450473",
+  // appId: "1:1004097450473:web:0c82f656fc99881ff89b78"
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore()
+export const db = getFirestore(app)
